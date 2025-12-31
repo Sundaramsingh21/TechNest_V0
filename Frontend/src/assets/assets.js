78e615518588b47c1c0965dbad6df97ee9cfba94
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2025-12-31 20:18:54 +0530

    Add files via upload

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,168 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+import menu_9 from './menu_9.png'
+import menu_10 from './menu_10.png'
+import menu_11 from './iot.png'
+import menu_12 from './gsm-gprs-module.png'
+import menu_13 from './battery.jpg'
+import menu_14 from './raspi.jpg'
+import menu_15 from './motor.png'
+import menu_16 from './pcb.png'
+import menu_17 from './ic.png'
+import menu_18 from './transformer.png'
+import menu_19 from './Display.png'
+import menu_20 from './dev.png'
+import menu_21 from './prog.png'
+import menu_22 from './project.jpeg'
+
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+import QR_code from './QR-code.png'
+import right_arrow from './right-arrow.png'
+import Phone_pay from './Phone_pe.png'
+import Googlepay from './Googlepay.png'
+import Copy from './copy.png'
+import picture_loader from './picture_loader.png'
+
+export const assets = {
+    Phone_pay,
+    Copy,
+    Googlepay,
+    logo,
+    right_arrow,
+    QR_code,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon,
+    picture_loader
+}
+
+export const menu_list = [
+    {
+        menu_name: "Projects",
+        menu_image: menu_22
+    },
+    {
+        menu_name: "Capacitors",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "LEDs",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Resistors",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Arduino Boards",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Sensors",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Wires",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Switches",
+        menu_image: menu_8
+    },
+    {
+        menu_name: "Transistors",
+        menu_image: menu_9
+    },
+    {
+        menu_name: "Diodes",
+        menu_image: menu_10
+    },
+    {
+        menu_name: "IOT & Wireless",
+        menu_image: menu_11
+    },
+    {
+        menu_name: "Development Boards",
+        menu_image: menu_20
+    },
+    {
+        menu_name: "GSM & GPS",
+        menu_image: menu_12
+    },
+    {
+        menu_name: "Battery",
+        menu_image: menu_13
+    },
+    {
+        menu_name: "Raspberry Pi Boards",
+        menu_image: menu_14
+    },
+    {
+        menu_name: "Motors",
+        menu_image: menu_15
+    },
+    {
+        menu_name: "PCB Boards",
+        menu_image: menu_16
+    },
+    {
+        menu_name: "Display",
+        menu_image: menu_19
+    },
+    {
+        menu_name: "Programmers",
+        menu_image: menu_21
+    },
+    {
+        menu_name: "Transformers",
+        menu_image: menu_18
+    },
+    {
+        menu_name: "IC's",
+        menu_image: menu_17
+    },
+    {
+        menu_name:"other",
+        menu_image: menu_1
+    }
+]
